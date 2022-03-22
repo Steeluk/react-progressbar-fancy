45c1d002fcc057062af3d37081d1aa44eedee439
Victor Reviejo <victor.reviejo@softgames.de>
2022-03-22 17:54:41 +0100

    Tweak size, shine, particles and add dividers.

--- src/ProgressBar/ProgressBar.tsx
+++ src/ProgressBar/ProgressBar.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import "./progressBar.scss";
 import { useEffect } from "react";
-import {IProgressBarProps} from "./ProgressBar.types";
-
-
+import { IProgressBarProps } from "./ProgressBar.types";
 
 const ProgressBar: React.FC<IProgressBarProps> = (props) => {
-  const [progressAnimation, setProgressAnimation] = React.useState<boolean>(
-    false
-  );
+  const [progressAnimation, setProgressAnimation] =
+    React.useState<boolean>(false);
 
-  const { score, progressWidth, className, primaryColor, secondaryColor, progressColor = primaryColor || secondaryColor ? '' : 'red', label, hideText, darkTheme,  disableGlow } = props;
+  const {
+    score,
+    progressWidth,
+    className,
+    primaryColor,
+    secondaryColor,
+    progressColor = primaryColor || secondaryColor ? "" : "red",
+    label,
+    hideText,
+    darkTheme,
+    disableGlow,
+  } = props;
 
   useEffect(() => {
     setProgressAnimation(true);
@@ -19,43 +27,79 @@ const ProgressBar: React.FC<IProgressBarProps> = (props) => {
   const renderProgressFiller = (glow: boolean) => (
     <>
       <div
-        className={`progressFill ${!(primaryColor || secondaryColor) && progressColor} ${
-          glow && 'glowingEffect'
-        }`}
+        className={`progressFill ${
+          !(primaryColor || secondaryColor) && progressColor
+        } ${glow && "glowingEffect"}`}
         style={{
           width: progressAnimation ? `${score}%` : 0,
           background: `linear-gradient( to right, ${primaryColor}, ${secondaryColor})`,
-        }} 
+        }}
       />
-      <div
-        className={`${!(primaryColor || secondaryColor) && progressColor}`}
-      >
-        <div className={`particlesContainer`} style={{
-          left: progressAnimation ?`${score > 1 ? score : 1}%` : 1,
-        }}>
+      {/* <div className="or-spacer-vertical left">
+        <div className="mask"></div>
+      </div> */}
+      <div className="vl1"></div>
+      <div className="vl2"></div>
+      <div className="vl3"></div>
+      <div className={`${!(primaryColor || secondaryColor) && progressColor}`}>
+        <div
+          className={`particlesContainer`}
+          style={{
+            left: progressAnimation ? `${score > 1 ? score : 1}%` : 1,
+          }}
+        >
           <div className={`particles`} />
-          <div className={`smallParticles smallParticles4`} style={{background: secondaryColor}} />
-          <div className={`smallParticles smallParticles3`} style={{background: secondaryColor}} />
-          <div className={`smallParticles smallParticles2`} style={{background: secondaryColor}} />
-          <div className={`bigParticles`} style={{background: secondaryColor}} />
-          <div className={`smallParticles`} style={{background: secondaryColor}} />
-          <div className={`particles particles2`} style={{background: secondaryColor}} />
-          <div className={`smallParticles smallParticles5`} style={{background: secondaryColor}} />
-          <div className={`smallParticles smallParticles6`} style={{background: secondaryColor}} />
+          <div
+            className={`smallParticles smallParticles4`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`smallParticles smallParticles3`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`smallParticles smallParticles2`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`bigParticles`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`smallParticles`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`particles particles2`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`smallParticles smallParticles5`}
+            style={{ background: secondaryColor }}
+          />
+          <div
+            className={`smallParticles smallParticles6`}
+            style={{ background: secondaryColor }}
+          />
         </div>
       </div>
     </>
   );
 
   return (
-    <div className={`progressBarFancyContainer ${className}`} style={{width: progressWidth}}>
-      {!hideText && <div className={`labelScoreContainer ${darkTheme && `labelDarkTheme`}`}>
-        <div className={`label`}>{label}</div>
-        <div className={`number`}>{score}%</div>
-      </div>}
+    <div
+      className={`progressBarFancyContainer ${className}`}
+      style={{ width: progressWidth }}
+    >
+      {!hideText && (
+        <div className={`labelScoreContainer ${darkTheme && `labelDarkTheme`}`}>
+          <div className={`label`}>{label}</div>
+          <div className={`number`}>{score}%</div>
+        </div>
+      )}
       <div className={`barGaugeContainer`} style={{ flexWrap: "wrap" }}>
         <div
-            className={`progressbarWidth`}
+          className={`progressbarWidth`}
           style={{
             width: progressWidth,
             margin: "0px 10px",
